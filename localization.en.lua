27ba93d92b6fd11cca3064e1a4165cca994e3a04
Gennady Kovalev <gik@bigur.ru>
2017-06-29 01:50:40 +0300

    Add eng locale file

---
+++ localization.en.lua
@@ -0,0 +1,10 @@
+-- Default locale
+
+GT_CONFIG_TITLE            = "Guild Taxes"
+GT_CONFIG_LOGGING_TITLE    = "Logging options"
+GT_CONFIG_LOGGING_LOG      = "Print transactions"
+GT_CONFIG_LOGGING_LOG_DESC = "Will print all transactions to chat from every loot"
+GT_CONFIG_VERBOSE_LOG      = "Verbose output"
+GT_CONFIG_VERBOSE_LOG_DESC = "Will print information received from guildmates"
+
+GT_CHAT_PREFIX             = "GuildTaxes"
