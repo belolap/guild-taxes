34850843f231aa3bc1bdc22fe41519a1433c4e03
Gennady Kovalev <gik@bigur.ru>
2017-06-28 18:17:29 +0300

    Add LibStub & fix communication stub function

--- GuildTaxes.lua
+++ GuildTaxes.lua
@@ -11,7 +11,7 @@ local CHAT_PREFIX = "|cffb0c4de" .. GT_CHAT_PREFIX .. ":|r "
 local MESSAGE_PREFIX = "GT"
 
 -- Instantiate
-GuildTaxes = LibStub("AceAddon-3.0"):NewAddon("GuildTaxes", "AceConsole-3.0", "AceEvent-3.0")
+GuildTaxes = LibStub("AceAddon-3.0"):NewAddon("GuildTaxes", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0")
 
 
 ------------------------
@@ -19,18 +19,17 @@ GuildTaxes = LibStub("AceAddon-3.0"):NewAddon("GuildTaxes", "AceConsole-3.0", "A
 ------------------------
 function GuildTaxes:OnInitialize()
 	self.db = LibStub("AceDB-3.0"):New("GuildTaxesDB")
-	self:RegisterComm(MESSAGE_PREFIX)
 end
 
 
 ------------------------
 -- Utilites
 ------------------------
-function GuildTaxes:logTransaction()
+function GuildTaxes:LogTransaction()
 end
 
 
-function GuildTaxes:debug(message, n)
+function GuildTaxes:Debug(message, n)
 	if (DEVELOPMENT or self.debugging) then
 		self:Print(CHAT_PREFIX .. "... " .. message)
 	end
@@ -40,16 +39,16 @@ end
 ------------------------
 -- Slash command
 ------------------------
-function GuildTaxes:onSlashCommand(input)
-	self:debug("Slash command: " .. input)
+function GuildTaxes:OnSlashCommand(input)
+	self:Debug("Slash command: " .. input)
 end
 
 
 ------------------------
 -- Communication events
 ------------------------
-function GuildTaxes:onCommReceived(prefix, message, destribution, sender)
-	self:debug("Communication message recieved")
+function GuildTaxes:OnCommReceived(prefix, message, distribution, sender)
+	self:Debug("Communication message recieved")
 end
 
 
@@ -57,38 +56,39 @@ end
 -- WoW events handlers
 ------------------------
 function GuildTaxes:PLAYER_ENTERING_WORLD()
-	self:debug("Player entering world")
+	self:Debug("Player entering world")
 end
 
 function GuildTaxes:PLAYER_MONEY( ... )
-	self:debug("Player money")
+	self:Debug("Player money")
 end
 
 function GuildTaxes:GUILDBANKFRAME_OPENED( ... )
-	self:debug("Guild bank opened")
+	self:Debug("Guild bank opened")
 end
 
 function GuildTaxes:GUILDBANKFRAME_CLOSED( ... )
-	self:debug("Guild bank closed")
+	self:Debug("Guild bank closed")
 end
 
 function GuildTaxes:MAIL_SHOW( ... )
-	self:debug("Open mailbox")
+	self:Debug("Open mailbox")
 end
 
 function GuildTaxes:MAIL_CLOSED( ... )
-	self:debug("Close mailbox")
+	self:Debug("Close mailbox")
 end
 
 function GuildTaxes:PLAYER_GUILD_UPDATE(unit)
-	self:debug("Player change guild")
+	self:Debug("Player change guild")
 end
 
 
 ------------------------
 -- Register events
 ------------------------
-GuildTaxes:RegisterChatCommand(SLASH_COMMAND, "onSlashCommand")
+GuildTaxes:RegisterChatCommand(SLASH_COMMAND, "OnSlashCommand")
+GuildTaxes:RegisterComm(MESSAGE_PREFIX)
 GuildTaxes:RegisterEvent("PLAYER_ENTERING_WORLD")
 GuildTaxes:RegisterEvent("PLAYER_MONEY")
 GuildTaxes:RegisterEvent("GUILDBANKFRAME_OPENED")
