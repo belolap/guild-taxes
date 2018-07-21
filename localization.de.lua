d019a4632ab2d7721a5edf77356fd45860b1dd08
Gennady Kovalev <gik@bigur.ru>
2018-07-21 14:59:25 +0300

    Fix german translation

--- localization.de.lua
+++ localization.de.lua
@@ -1,60 +1,35 @@
 if GetLocale() ~= "deDE" then return end
-
---Translation missing
-GT_CHAT_ALL_PAYED = "Всё оплачено, спасибо!"
---Translation missing
-GT_CHAT_GENERAL_INFO = "Платим |cffffff00%i%%|r налогов в гильдию |cffffff00%s|r@|cffffff00%s|r"
+GT_CHAT_ALL_PAYED = "Alles bezahlt, danke!"
+GT_CHAT_GENERAL_INFO = "Es fallen |cffffff00%i%%|r Steuern zugunsten der Gilde |cffffff00%s|r@|cffffff00%s|r an."
 GT_CHAT_NOT_READY = "Addon noch nicht bereit, versuch es später nochmal."
 GT_CHAT_NOTHING_TO_PAY = "Nichts zu bezahlen."
---Translation missing
-GT_CHAT_OPEN_BANK = "Пожалуйста, откройте банк гильдии"
---Translation missing
-GT_CHAT_PAYING_TAX = "Оплачиваю %s налога"
---Translation missing
-GT_CHAT_PREFIX = "Налоги"
+GT_CHAT_OPEN_BANK = "Bitte öffne die Gildenbank!"
+GT_CHAT_PAYING_TAX = "Zahle %s Steuer"
+GT_CHAT_PREFIX = "GuildTaxes"
 GT_CHAT_TAX = "Zu bezahlen: %s"
---Translation missing
-GT_CHAT_TAX_CLICK = "Нажмите для оплаты"
---Translation missing
-GT_CHAT_TRANSACTION = "Получено %s (налог %s)"
---Translation missing
-GT_CONFIG_DEBUG_LOG = "Отладка"
---Translation missing
-GT_CONFIG_DEBUG_LOG_DESC = "Печать отладочной информации (для разработчиков)"
---Translation missing
-GT_CONFIG_LOGGING_LOG = "Печать начислений"
---Translation missing
-GT_CONFIG_LOGGING_LOG_DESC = "Начисления налогов с каждого лута будут печататься в чат"
---Translation missing
-GT_CONFIG_LOGGING_TITLE = "Настройки чата"
---Translation missing
-GT_CONFIG_TAXES_AUTOPAY = "Платить автоматически"
---Translation missing
-GT_CONFIG_TAXES_AUTOPAY_DESC = "Оплата налогов будет происходить автоматически при открытии банка гильдии"
---Translation missing
-GT_CONFIG_TAXES_DIRECT = "Учитывать ручные взносы"
---Translation missing
-GT_CONFIG_TAXES_DIRECT_DESC = "Ручные взносы в банк будут учитываться как уплата налогов"
---Translation missing
-GT_CONFIG_TAXES_RANGE = "Процентная ставка"
---Translation missing
-GT_CONFIG_TAXES_RANGE_DESC = "Процент от суммы всего лута, который будет начисляться для оплаты в банк гильдии"
---Translation missing
-GT_CONFIG_TAXES_TITLE = "Настройки налогообложения"
---Translation missing
-GT_CONFIG_TITLE = "Налоги гильдии"
---Translation missing
-GT_CONFIG_VERBOSE_LOG = "Информация от согильдейцев"
---Translation missing
-GT_CONFIG_VERBOSE_LOG_DESC = "В чат будет печататься информация о транзакциях согильдейцев"
---Translation missing
-GT_GUI_ALL_PAYED = "Всё оплачено"
+GT_CHAT_TAX_CLICK = "Klicken, um zu zahlen"
+GT_CHAT_TRANSACTION = "Erhalten %s (Steuer %s)"
+GT_CONFIG_DEBUG_LOG = "Debuggen"
+GT_CONFIG_DEBUG_LOG_DESC = "Debugging-Informationen anzeigen (für Entwickler)"
+GT_CONFIG_LOGGING_LOG = "Chatfenster Anzeige"
+GT_CONFIG_LOGGING_LOG_DESC = "Die Steuern von jeder Beute werden im Chatfenster angezeit"
+GT_CONFIG_LOGGING_TITLE = "Anzeigeeinstellungen"
+GT_CONFIG_TAXES_AUTOPAY = "Automatisch bezahlen"
+GT_CONFIG_TAXES_AUTOPAY_DESC = "Steuern werden automatisch beim Öffnen der Gildenbank eingezahlt"
+GT_CONFIG_TAXES_DIRECT = "Beachte manuelle Einzahlungen"
+GT_CONFIG_TAXES_DIRECT_DESC = "Manuelle Einzahlungen an die Bank werden als Steuerzahlungen erfasst"
+GT_CONFIG_TAXES_RANGE = "Steuersatz"
+GT_CONFIG_TAXES_RANGE_DESC = "Prozentsatz des Gesamtbetrags der Beute, der für die Zahlung an die Gildenbank aufgelaufen ist"
+GT_CONFIG_TAXES_TITLE = "Steuereinstellungen"
+GT_CONFIG_TITLE = "Gildensteuern"
+GT_CONFIG_VERBOSE_LOG = "Informationen von Gildenmitgliedern"
+GT_CONFIG_VERBOSE_LOG_DESC = "Im Chat werden Informationen über die Transaktionen von Mitgliedern angezeigt"
+GT_GUI_ALL_PAYED = "Alles bezahlt"
 GT_GUI_COL_NAME = "Name"
 GT_GUI_COL_RANK = "Rang"
 GT_GUI_COL_TAX = "Steuer"
 GT_GUI_COL_TOTAL = "Gesamt"
---Translation missing
-GT_GUI_GENERAL_INFO = ", платим |cffffff00%i%%|r в гильдию |cffffff00%s|r"
+GT_GUI_GENERAL_INFO = ", aktuell gehen |cffffff00%i%%|r in die GildenBank von |cffffff00%s|r"
 GT_GUI_MONTH_1 = "Jan"
 GT_GUI_MONTH_10 = "Okt"
 GT_GUI_MONTH_11 = "Nov"
@@ -67,9 +42,6 @@ GT_GUI_MONTH_6 = "Jun"
 GT_GUI_MONTH_7 = "Jul"
 GT_GUI_MONTH_8 = "Aug"
 GT_GUI_MONTH_9 = "Sep"
---Translation missing
-GT_GUI_ONLINE_ONLY = "Только онлайн"
---Translation missing
-GT_GUI_TAX = "Начислено %s"
---Translation missing
-GT_GUI_TITLE = "Налоги гильдии"
+GT_GUI_ONLINE_ONLY = "Nur Online"
+GT_GUI_TAX = "Steuerschulden: %s"
+GT_GUI_TITLE = "Gildensteuern"
